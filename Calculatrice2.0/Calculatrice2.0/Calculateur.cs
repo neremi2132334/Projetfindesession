13bf24eb9ec5c97c8fb12ad7e89b613afac0a839
Nathan Savard <2233318@etu.cegepjonquiere.ca>
2023-04-17 14:20:56 -0400

    Calculatrice Erreur Manquante

--- Calculatrice2.0/Calculatrice2.0/Calculateur.cs
+++ Calculatrice2.0/Calculatrice2.0/Calculateur.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace Calculatrice2._0
 {
-    internal class Calculateur
+    partial class Calculateur
     {
+        //Fonction Addition
+        public double Addition(int x, int y)
+        {
+            double total = x + y;
+            return total;
+        }
+
+        //Fonction Soustraction
+        public double Soustraction(int x, int y)
+        {
+            double total = x - y;
+            return total;
+        }
+
+        //Fonction Divsion
+        public double Diviser(int x, int y)
+        {
+            double total = x / y;
+            return total;
+        }
+
+        //Fonction Multiplication
+        public double Multiplier(int x, int y)
+        {
+            double total = x * y;
+            return total;
+        }
+
+
+        //Fonction Log
+        public double Exposant(int x, int y)
+        {
+            double z;
+            z = Math.Pow(x, y);
+            return z;
+        }
+
+        //Fonction Log
+        public double Log(int x, int y)
+        {
+            double total = Math.Log(x,y);
+            return total;
+        }
+
+
+
+
     }
 }
--- Calculatrice2.0/Calculatrice2.0/Program.cs
+++ Calculatrice2.0/Calculatrice2.0/Program.cs
@@ -14,30 +14,41 @@ namespace Calculatrice2._0
             {
                 int x = 0;
                 int y = 0;
-                double total;
+                double total=0;
                 Console.WriteLine("ENTREZ UN 1er CHIFFRE (X).");
                 x = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("ENTREZ UN 2eme CHIFFRE (Y).");
                 y = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Menu d'Action" +
-                    "(A) Addition" +
-                    "(S) Soustraction" +
-                    "(D) Division" +
-                    "(M) Multiplication" +
-                    "(E) Exposant" +
-                    "(L) Logarithme");
-                string choix = Console.ReadLine();
-                if(choix == A)
-                {
-                    Console.WriteLine("FONCTION ADDITION");
-                }
-
-
-
-                total = calculatrice.Diviser(x, y);
-                Console.WriteLine($"La Réponse de la divsion ente {x} et {y} est égale a {total}");
-                total = calculatrice.Multiplier(x, y);
-                Console.WriteLine($"La Réponse de la Multiplication ente {x} et {y} est égale a {total}");
+                    "(1) Addition" +
+                    "(2) Soustraction" +
+                    "(3) Division" +
+                    "(4) Multiplication" +
+                    "(5) Exposant" +
+                    "(6) Logarithme");
+                int choix = Convert.ToInt32(Console.ReadLine());
+                    switch (choix)
+                    {
+                        case 1:
+                            total =calculatrice.Addition(x,y);
+                            break;
+                        case 2:
+                            total = calculatrice.Soustraction(x, y);
+                            break;
+                        case 3:
+                            total = calculatrice.Diviser(x, y);
+                            break;
+                        case 4:
+                            total = calculatrice.Multiplier(x, y);
+                            break;
+                        case 5:
+                            total= calculatrice.Exposant(x,y);
+                            break;
+                        case 6:
+                            total = calculatrice.Log(x,y);
+                            break;
+                    }
+                Console.WriteLine($"{total}");
             }
             catch (DivideByZeroException ex)
             {
